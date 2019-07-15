b2469c00941bbe87b9bd3015c1b9ae71e3b26dcc
Ayush Shrivastava <ayush16024@iiitd.ac.in>
2019-07-15 21:50:44 +0530

    Added raw app with just title and scrollbar.

---
+++ app/src/main/java/com/example/ayush/timestableapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.ayush.timestableapp;
+
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SeekBar seekBar = findViewById(R.id.seekBar3);
+        seekBar.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+        seekBar.getThumb().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+    }
+}
